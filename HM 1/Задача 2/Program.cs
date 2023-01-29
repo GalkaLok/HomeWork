b2429ded35087316f0e6391db631df5918aea46c
Galina Loktikova <gloktikova@gmail.com>
2023-01-29 21:33:32 +0300

    Решение задач

---
+++ HM 1/Задача 2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Ведите первое число: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.Write("Ведите второе число: ");
+int B = int.Parse(Console.ReadLine()!);
+
+if (A > B)
+{
+    Console.WriteLine($"max = {A}");
+}
+else
+{
+    Console.WriteLine($"max = {B}");
+}
---
+++ HM 1/Задача 4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Ведите первое число: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.Write("Ведите второе число: ");
+int B = int.Parse(Console.ReadLine()!);
+Console.Write("Ведите третье число: ");
+int C = int.Parse(Console.ReadLine()!);
+
+int max = A;
+
+if (B > max)
+{
+    int max = B;
+}
+else if (C > max)
+{
+    int max = C;
+}
+
+Console.WriteLine($"Максимальное число = {max}");
