b5b5884c6f03162de05caa60733498a80e006eea
Galina Loktikova <gloktikova@gmail.com>
2023-02-12 01:47:55 +0300

    Задачи семинар 3

---
+++ HM_3/Задача_21/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.Clear();
+Console.Write("Введите X первой точки: ");
+int X1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите Y первой точки: ");
+int Y1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите Z первой точки: ");
+int Z1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите X второй точки: ");
+int X2 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите Y второй точки: ");
+int Y2 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите Z второй точки: ");
+int Z2 = int.Parse(Console.ReadLine()!);
+
+double x = Math.Pow(X1-X2,2);
+double y = Math.Pow(Y1-Y2,2);
+double z = Math.Pow(Z1-Z2,2);
+
+double c = Math.Sqrt (x+y+z);
+
+Console.WriteLine($"Длина равна {c:f2}");
---
+++ HM_3/Задача_23/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+int count = 1;
+
+while (count <= N)
+{
+    Console.Write(count * count* count);
+    if (count != N)
+    {
+        Console.Write(", ");
+    }
+
+    count++;
+}
