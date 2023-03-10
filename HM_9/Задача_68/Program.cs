93d20a3120d83a62241bf998551287949b0f0e39
Galina Loktikova <gloktikova@gmail.com>
2023-03-10 20:15:06 +0300

    HM9

--- HM_9/Задача_68/Program.cs
+++ HM_9/Задача_68/Program.cs
@@ -6,15 +6,9 @@ Console.Clear();
 int Recurs(int m, int n)
 {
     if (m == 0) return n + 1;
-    else if (n == 0 && m != 0)
-    {
-        return Recurs(m - 1, 1);
-    }
-    else if (n != 0 && m != 0)
-    {
-        return Recurs(m-1, Recurs(m,n-1));
-    }
-
+    else if (n == 0 && m != 0) return Recurs(m - 1, 1);
+    else if (n != 0 && m != 0) return Recurs(m - 1, Recurs(m, n - 1));
+    return Recurs(m, n);
 }
 
 Console.Write("Введите число m: ");
