724e5fa9add3b4a6a8f43b1ad438b2590c4ea431
Galina Loktikova <gloktikova@gmail.com>
2023-03-10 20:11:39 +0300

    HM9

---
+++ HM_9/Задача_64/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте значение N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.Clear();
+
+string Recurs(int N)
+{
+    if (N == 1) return N.ToString();
+    else
+    {
+        return N.ToString() + ", " + Recurs(N - 1);
+    }
+}
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Recurs(N));
---
+++ HM_9/Задача_66/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значения M и N. Напишите программу, 
+//которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.Clear();
+
+int Recurs(int N, int M)
+{
+    if (N == M) return N;
+    else
+    {
+        return Recurs(N - 1, M) + N;
+    }
+}
+
+Console.Write("Введите число M: ");
+int M = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(Recurs(N, M));
